94b1f5812267a82a48ba2c0f33f7226c99fc1c19
unknown <f.rakotoasimbola@gmail.com>
2022-05-26 10:31:14 +0200

    ci: adding env files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+    swcMinify: false,
+    presets: ['next/babel'],
+    env: { TEST: process.env.TEST },
+    async rewrites() {
+        return [];
+    },
+};
